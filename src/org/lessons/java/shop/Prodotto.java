c682df13012200185dd921e48cba89b6dafbf612
FrancescoGuggino <landongu@gmail.com>
2023-07-20 14:28:15 +0200

    commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,61 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+    // campi
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private String prezzo;
+    private String iva;
+
+    // costruttore
+
+    public Prodotto(int codice, String nome, String descrizione, String prezzo, String iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // getter e setter
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getIva() {
+        return iva;
+    }
+
+    public void setIva(String iva) {
+        this.iva = iva;
+    }
+
+    // metodi
+}
