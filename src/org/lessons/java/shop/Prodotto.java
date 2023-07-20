dcf344b37145235b3ab5f67b51fb26be5fb60e0c
FrancescoGuggino <landongu@gmail.com>
2023-07-20 14:43:51 +0200

    Prodotto 1 add metodi

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -6,12 +6,12 @@ public class Prodotto {
     private int codice;
     private String nome;
     private String descrizione;
-    private String prezzo;
-    private String iva;
+    private double prezzo;
+    private double iva;
 
     // costruttore
 
-    public Prodotto(int codice, String nome, String descrizione, String prezzo, String iva) {
+    public Prodotto(int codice, String nome, String descrizione, double prezzo, double iva) {
         this.codice = codice;
         this.nome = nome;
         this.descrizione = descrizione;
@@ -41,21 +41,33 @@ public class Prodotto {
         this.descrizione = descrizione;
     }
 
-    public String getPrezzo() {
+    public double getPrezzo() {
         return prezzo;
     }
 
-    public void setPrezzo(String prezzo) {
+    public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
     }
 
-    public String getIva() {
+    public double getIva() {
         return iva;
     }
 
-    public void setIva(String iva) {
+    public void setIva(double iva) {
         this.iva = iva;
     }
 
     // metodi
+
+
+    @Override
+    public String toString() {
+        return "Prodotto{" +
+                "codice=" + codice +
+                ", nome='" + nome +
+                ", descrizione='" + descrizione +
+                ", prezzo=" + prezzo +
+                ", iva=" + iva +
+                '}';
+    }
 }
